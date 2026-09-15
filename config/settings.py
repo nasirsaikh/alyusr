d969a62013590ad4f9a4e8591ae4a4638cd0a0f5
nasirsaikh <nasirsaikh@gmail.com>
2026-09-15 13:52:53 +0400

    Use source-map tolerant WhiteNoise manifest storage

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {
     "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
-    "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
+    "staticfiles": {"BACKEND": "config.storage.AlyusrStaticFilesStorage"},
 }
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
