0648bae803dd76dffe92af5602fa37565da1d1ed
nasirsaikh <nasirsaikh@gmail.com>
2026-09-15 14:28:04 +0400

    Register Alyusr template utility filters

--- config/settings.py
+++ config/settings.py
@@ -75,16 +75,19 @@ TEMPLATES = [{
     "BACKEND": "django.template.backends.django.DjangoTemplates",
     "DIRS": [BASE_DIR / "templates"],
     "APP_DIRS": True,
-    "OPTIONS": {"context_processors": [
-        "django.template.context_processors.request",
-        "django.contrib.auth.context_processors.auth",
-        "django.contrib.messages.context_processors.messages",
-        "django.template.context_processors.i18n",
-        "django.template.context_processors.static",
-        "cms.context_processors.cms_settings",
-        "sekizai.context_processors.sekizai",
-        "apps.common.context_processors.platform_context",
-    ]},
+    "OPTIONS": {
+        "builtins": ["apps.common.templatetags.common_extras"],
+        "context_processors": [
+            "django.template.context_processors.request",
+            "django.contrib.auth.context_processors.auth",
+            "django.contrib.messages.context_processors.messages",
+            "django.template.context_processors.i18n",
+            "django.template.context_processors.static",
+            "cms.context_processors.cms_settings",
+            "sekizai.context_processors.sekizai",
+            "apps.common.context_processors.platform_context",
+        ],
+    },
 }]
 WSGI_APPLICATION = "config.wsgi.application"
 ASGI_APPLICATION = "config.asgi.application"
